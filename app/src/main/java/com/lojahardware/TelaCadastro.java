3e7c87e90ad2cd28b1d0778c91b830882bbb4c55
Wesley <94919129+IkfesSekfes@users.noreply.github.com>
2022-06-08 12:17:58 -0300

    Tela cadastro

---
+++ app/src/main/java/com/lojahardware/TelaCadastro.java
@@ -0,0 +1,27 @@
+package com.lojahardware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.os.Bundle;
+
+import java.util.logging.SimpleFormatter;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+
+
+    }
+
+    public void confirmarCardastro(View view) {
+
+
+
+
+    }
+}
